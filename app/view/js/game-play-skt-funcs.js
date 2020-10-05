52baab88273efa2882e36ffb600fae4a6ff2658d
a-iannuzzi <andrew.iannuzzi@wondermakr.com>
2020-10-05 13:35:43 -0400

    winner videos

--- app/view/js/game-play-skt-funcs.js
+++ app/view/js/game-play-skt-funcs.js
@@ -7,18 +7,19 @@ $(document).ready(function () {
 		$('#flow-screens h1').text(name);
 		$('#flow-screens #putt-message').html('Putt<br><span class="lrg">3 Balls</span>');
 		$('#flow-screens').fadeIn(500);
+		//$('#flow-screens').append('<div id="shots"><div id="s1" class="golf-ball"></div><div id="s2" class="golf-ball"></div><div id="s3" class="golf-ball"></div></div>');
 	}
 	
 	
 	function displayScoreAndMessageWithName(name, score, message) {
 		
 		$('#flow-screens #putt-score').fadeOut(500);
-		$('#flow-screens #putt-message').fadeOut(400, function() {
+		$('#flow-screens #putt-message').fadeOut(500, function() {
 			$('#flow-screens').removeClass();
 			$('#flow-screens h1').text(name);
 			$('#flow-screens #putt-score').html(score+' <span>for</span> 3');
 			$('#flow-screens #putt-message').html(message);
-			$('#putt-score, #putt-message').fadeIn(500);
+			$('#flow-screens, #putt-score, #putt-message').fadeIn(500);
 		});
 	}
 	
@@ -26,6 +27,24 @@ $(document).ready(function () {
 		
 		var $player = $('.p'+pNum+'.player');
 		displayScoreAndMessageWithName($player.children('.name').text(), score, message);
+	}
+	
+	function displayGreatPuttWithName(name) {
+		$('#putt-score').fadeOut(500);
+		$('#flow-screens #putt-message').fadeOut(500, function() {
+			$('#flow-screens').removeClass().addClass('greatShot');
+			$('#flow-screens h1').text(name);
+			$('#flow-screens #putt-message').addClass('b4-animate').html('<span class="lrg">Great<br>Shot!</span>').show().addClass('animate');
+			$('#flow-screens').fadeIn(500);
+			setTimeout(function() {
+				$('#flow-screens #putt-message').removeClass('b4-animate');
+			}, 500);
+		});
+	}
+	
+	function displayGreatPuttWithPlayerNum(pNum) {
+		var $player = $('.p'+pNum+'.player');
+		displayGreatPuttWithName($player.children('.name').text());
 	}
 	
 	function removeUserFlowScreen() {
@@ -37,21 +56,25 @@ $(document).ready(function () {
 	}
 	
 	/* Tests */
-	if (cfg_screen == 'small') {
-		setTimeout(function() {
-			//socket.emit('game-play-alert', {func: 'displayScoreAndMessageWithName', params : ['Andrew', '0', 'Better Luck<br>Next Time']});
-			//socket.emit('game-play-alert', {func: 'displayScoreAndMessageWithPlayerNum', params : [1, '1', 'One is better<br>than nothing']});
-			socket.emit('game-play-alert', {func: 'displayScoreAndMessageWithName', params : ['Andrew', '2', "Way to go!<br>Almost Perfect"]});
-			//socket.emit('game-play-alert', {func: 'displayScoreAndMessageWithPlayerNum', params : [2, '3', "We got a<br>pro over here!"]});
-		}, 5000);
+	if (cfg_screen == 'smalll') {
 		setTimeout(function() {
 			//socket.emit('game-play-alert', {func: 'removeUserFlowScreen', params : []});
 			socket.emit('game-play-alert', {func: 'tellSomeoneToPutt', params : ['Andrew']});
 		}, 1000);
+		setTimeout(function() {
+			socket.emit('game-play-alert', {func: 'removeUserFlowScreen', params : []});
+		}, 3000);
+		setTimeout(function() {
+			//socket.emit('game-play-alert', {func: 'displayScoreAndMessageWithName', params : ['Andrew', '0', 'Better Luck<br>Next Time']});
+			//socket.emit('game-play-alert', {func: 'displayScoreAndMessageWithPlayerNum', params : [1, '1', 'One is better<br>than nothing']});
+			//socket.emit('game-play-alert', {func: 'displayScoreAndMessageWithName', params : ['Andrew', '2', "Way to go!<br>Almost Perfect"]});
+			//socket.emit('game-play-alert', {func: 'displayScoreAndMessageWithPlayerNum', params : [2, '3', "We got a<br>pro over here!"]});
+		}, 5000);
+		setTimeout(function() {
+			//socket.emit('game-play-alert', {func: 'displayGreatPuttWithName', params : ['Andrew']});
+			socket.emit('game-play-alert', {func: 'displayGreatPuttWithPlayerNum', params : [1]});
+		}, 6000);
 	}
-	//setTimeout(function() {displayScoreAndMessageWithName('putt3', 'Andrew')}, 1000);
-	//setTimeout(function() {displayScoreAndMessageWithPlayerNum('putt3', 1)}, 1000);
-	//setTimeout(function() {removeUserFlowScreen()}, 3000);
 	
 	
 	
@@ -79,18 +102,6 @@ $(document).ready(function () {
 			socket.emit('game-play-alert', {func: 'changeHoleColor', params : ['1', 'red']});
 		}, 4000);
 	}
-	/*
-	setTimeout(function() {
-		changeHoleColor(1,'green');
-		changeHoleColor(2,'yellow');
-		changeHoleColor(3,'red');
-	}, 1000);
-	
-	setTimeout(function() {
-		changeAllHoleColors('red');
-	}, 3000);
-	*/
-	
 	
 	
 	var videos = ['fireworks', 'rocket', 'shark', 'trophy', 'wrecking-ball'];
@@ -109,13 +120,41 @@ $(document).ready(function () {
 		video.onloadeddata = function() {
 			$('#winner-container').fadeIn(500, function() {
 				video.play();
+				$('#winner-is h1').text(winnerName);
+				var winner_timeout;
+				switch(videoName) {
+					
+					case 'fireworks':
+						winner_timeout = 7000;
+					break;
+					
+					case 'rocket':
+						winner_timeout = 3000;
+					break;
+					
+					case 'shark':
+					case 'trophy':
+						winner_timeout = 5500;
+					break;
+					
+					case 'wrecking-ball':
+						winner_timeout = 4500;
+					break;
+					
+					default:
+						winner_timeout = 10000;
+					break;
+				}
+				
+				setTimeout(function() {
+					$('#winner-is').fadeIn(1000);
+				}, winner_timeout);
+				
 			});
 		}
 		
 		video.onended = function() {
 			console.log('Video Ended');
-			$('#winner-is h1').text(winnerName);
-			$('#winner-is').fadeIn(500);
 		};
 		
 	}
@@ -126,12 +165,11 @@ $(document).ready(function () {
 	}
 	
 	/* TESTS */
-	if (cfg_screen == 'smalll') {
+	if (cfg_screen == 'small') {
 		setTimeout(function() {
 			socket.emit('game-play-alert', {func: 'displayWinnerWithPlayerNum', params : ['wrecking-ball', '1']});
-		}, 2000);
+		}, 1000);
 	}
-	//displayWinnerWithPlayerNum('rand', 2);
 	
 	
 	socket.on('game-play-alert', function(msg) {
@@ -157,6 +195,8 @@ $(document).ready(function () {
 			// One Parameter
 			case 'tellSomeoneToPutt':
 			case 'changeAllHoleColors':
+			case 'displayGreatPuttWithName':
+			case 'displayGreatPuttWithPlayerNum':
 				eval(msg.func +"('"+msg.params[0]+"')");
 			break;
 				
